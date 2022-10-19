3327f6b37e58be333ba73d055f59c55b3de16a5f
gregory <67207721+midilicious@users.noreply.github.com>
2022-10-19 16:36:21 -0400

    Create dieselnator.sh
    Created dieselnator

---
+++ dieselnator.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Takes $JOKE and replaces Chuck Norris with Vin Diesel
+
+dieselfy () {
+VINJOKE=$1 # takes $JOKE
+VIN=$(sed -e 's/Chuck/Vin/gI' $VINJOKE) # Replace Chuck with Vin
+DIESEL=$(sed -e 's/Norris/Diesel/gI' $VIN) # Replace Norris with Diesel
+$1=$DIESEL
+}
