037d329be38b17e2b09e3e7dda2dfeb65f7d0469
gregory <67207721+midilicious@users.noreply.github.com>
2022-10-19 17:36:17 -0400

    add jq dependency check and install

--- check_dependencies.sh
+++ check_dependencies.sh
@@ -41,3 +41,24 @@ check_cowsay () {
     fi
 }
 
+# Figures out if jq is installed, and installs if not.
+check_cowsay () {
+    if [[ $1 == "MAC" ]]; then
+        brew list -q jq > /dev/null 2>&1
+        status=$(echo $?)
+        if [[ $status == "1" ]]; then
+            brew install jq > /dev/null
+        fi
+    elif [[ $1 == "LINUX" ]]; then
+        if [[ "$uname_response" == *"Ubuntu"* ]]; then
+            dpkg -s jq > /dev/null 2>&1
+            status=$(echo $?)
+            if [[ $status == "1" ]]; then
+                sudo apt-get -y install jq > /dev/null 2>&1
+            fi
+        else
+            echo "We're currently only installing for Ubuntu & Darwin."
+            echo "Please check back later for updates."
+        fi
+    fi
+}
